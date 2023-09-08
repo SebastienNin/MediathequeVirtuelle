6e858082d7db004c79cea1732db683e82dcb572b
DianeR <diane.reja@gmail.com>
2023-09-08 14:08:09 +0200

    Debut spring boot

---
+++ mediatheque-virtuelle/mediatheque-virtuelle/src/main/java/mediatheque/MediathequeVirtuelleApplication.java
@@ -0,0 +1,13 @@
+package mediatheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediathequeVirtuelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediathequeVirtuelleApplication.class, args);
+	}
+
+}
---
+++ mediatheque-virtuelle/mediatheque-virtuelle/src/test/java/mediatheque/MediathequeVirtuelleApplicationTests.java
@@ -0,0 +1,13 @@
+package mediatheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediathequeVirtuelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

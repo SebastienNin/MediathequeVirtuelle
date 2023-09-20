c325a69341225eec6909ca0e36cdf83c794a6f24
Victorgm534 <victorgm534@live.com>
2023-09-20 11:55:58 +0200

    Modele

---
+++ angular/mediatheque_virtuelle/src/app/modele/media.ts
@@ -0,0 +1,9 @@
+export class media {
+    protected id: number;
+    protected name: string;
+    protected publishingHouse: string;
+    protected language: string;
+    protected image: string;
+    protected description: string;
+    protected dematerialized: boolean;
+}

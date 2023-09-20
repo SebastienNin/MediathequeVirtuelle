964f84abc7ec2c687d6f56f8b45e8c950a9ba1cc
SebastienNin <home-world@hotmail.Fr>
2023-09-20 14:33:42 +0200

    Add signin to appmodules

--- angular/mediatheque_virtuelle/src/app/app-routing.module.ts
+++ angular/mediatheque_virtuelle/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   {path: "user", component: UserComponent},
+  {path: "signin", component: SigninComponent},
   {path: "connexion", component: ConnexionComponent}
 ];
 

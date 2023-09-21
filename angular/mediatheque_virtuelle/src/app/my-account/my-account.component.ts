649d82477564a4634075b9fa7e5ae0ccd3727bc2
SebastienNin <home-world@hotmail.Fr>
2023-09-21 16:28:57 +0200

    Update my account component. Add password check and update password

--- angular/mediatheque_virtuelle/src/app/my-account/my-account.component.ts
+++ angular/mediatheque_virtuelle/src/app/my-account/my-account.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Account } from '../modele/account';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { AccountHttpService } from '../account/account-http.service';
 
 @Component({
   selector: 'app-my-account',
@@ -6,5 +10,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-account.component.scss']
 })
 export class MyAccountComponent {
+  user: Account;
+  selectedMenu: string;
 
+  editInfoFormGroup: FormGroup;
+  changePasswordFormGroup: FormGroup;
+
+  constructor(private authService: AuthService, private formBuilder: FormBuilder, private accountHttpService: AccountHttpService) {
+    this.user = this.authService.getUser();
+    this.selectedMenu = 'info'; // Par défaut, affichez les informations de l'utilisateur
+
+    this.editInfoFormGroup = this.formBuilder.group({
+      login: [this.user.login, Validators.required], // Exemple de validation avec Validators.required
+      name: [this.user.name, Validators.required],
+      firstName: [this.user.firstName, Validators.required],
+      mail: [this.user.mail, [Validators.required, Validators.email]]
+    });
+
+    this.changePasswordFormGroup = this.formBuilder.group({
+      currentPassword: ['', [Validators.required, this.validateCurrentPassword.bind(this)]],
+      newPassword: ['', Validators.required],
+      confirmNewPassword: ['', [Validators.required]]
+    });
+  }
+
+  selectMenu(menu: string) {
+    this.selectedMenu = menu;
+  }
+
+  updateUserInfo() {
+    const updatedUserInfo = this.editInfoFormGroup.value;
+    this.accountHttpService.save(updatedUserInfo).subscribe(
+      (response) => {
+        this.user = { ...this.user, ...updatedUserInfo };
+        console.log(this.user);
+        alert("Informations mises à jour avec succès.");
+      }
+    );
+  }
+
+  validateCurrentPassword(control: AbstractControl): ValidationErrors | null {
+    const currentPasswordFromForm = control.value;
+    if (currentPasswordFromForm) {
+      if (currentPasswordFromForm !== this.user.password)
+        return { invalidFormPassword: 'Le mot de passe ne correspond pas à votre mot de passe enregistré' };
+    }
+    return null; // Valide si tout est en ordre
+  }
+
+  updatePassword() {
+    this.user.password = this.changePasswordFormGroup.value.newPassword;
+    this.accountHttpService.save(this.user);
+    alert("Mot de passe mis à jour avec succès.");
+  }
 }

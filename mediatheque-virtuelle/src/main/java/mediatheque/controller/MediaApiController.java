8b7b58c9aed577028e65411fe7ddd95e948fba28
DianeR <diane.reja@gmail.com>
2023-09-21 19:03:52 +0200

    Correction CORS MediaApiController

--- mediatheque-virtuelle/src/main/java/mediatheque/controller/MediaApiController.java
+++ mediatheque-virtuelle/src/main/java/mediatheque/controller/MediaApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import mediatheque.model.VideoGame;
 import mediatheque.model.Views;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") 
 @RequestMapping("/api/media")
 public class MediaApiController {
 

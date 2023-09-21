615d09b935f155cac3bb0fa0af0c0a04d9819a95
Baptiste35136 <baptiste.guineheux@hotmail.fr>
2023-09-21 09:54:12 +0200

    persolistService

---
+++ angular/mediatheque_virtuelle/src/app/perso-list/perso-list.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { PersonnalizedList } from '../modele/personnalizedList';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersoListService {
+
+  constructor() { }
+}

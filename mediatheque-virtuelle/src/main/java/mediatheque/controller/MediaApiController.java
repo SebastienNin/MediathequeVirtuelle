6998257988d56038f16dcd3c4a6508f673247989
SebastienNin <home-world@hotmail.Fr>
2023-09-25 11:50:03 +0200

    Correct conflict in mediaapicontroller

--- mediatheque-virtuelle/src/main/java/mediatheque/controller/MediaApiController.java
+++ mediatheque-virtuelle/src/main/java/mediatheque/controller/MediaApiController.java
@@ -88,12 +88,12 @@ public class MediaApiController {
 		return daoMedia.findByName(name);
 	}
 
-//	@GetMapping("/{id}")
-//	@JsonView(Views.Media.class)
-//	public Media findMediaById(@PathVariable Integer id) {
-//		return daoMedia.findById(id).get();
-//	}
-	
+	// @GetMapping("/{id}")
+	// @JsonView(Views.Media.class)
+	// public Media findMediaById(@PathVariable Integer id) {
+	// return daoMedia.findById(id).get();
+	// }
+
 	@GetMapping("/nameContaining/{name}")
 	// To set back if needed | Seb
 	// @JsonView(Views.Media.class)
@@ -113,67 +113,15 @@ public class MediaApiController {
 	public MediaResponse findMediaById(@PathVariable Integer id) {
 		Media media = this.daoMedia.findById(id).orElseThrow(MediaNotFoundException::new);
 		MediaResponse response = new MediaResponse();
-		
+
 		BeanUtils.copyProperties(media, response);
-		
-		//A changer et adapter
-		//response.setNbProduits(fournisseur.getProduits().size());
+
+		// A changer et adapter
+		// response.setNbProduits(fournisseur.getProduits().size());
 		response.setNbAccountMedia(media.getAccountMediaList().size());
-		
+
 		return response;
-	// @JsonView(Views.Media.class)
-	public Media findMediaById(@PathVariable Integer id) {
-		return daoMedia.findById(id).get();
 	}
-
-	// @PostMapping("/")
-	// @JsonView(Views.Media.class)
-	// public Media createMedia(@Valid @RequestBody MediaRequest mediaRequest,
-	// BindingResult result) {
-	// if (result.hasErrors()) {
-	// throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Media invalide");
-	// }
-	// // Le mediaRequest récupéré est un type général qui possède un attribut
-	// // typeMedia qui permet de faire la distinction sur le type réel du média,
-	// // en fonction, on récupre les propriétés spécifiques au type de média
-	// concerné
-	// switch (mediaRequest.getTypeMedia()) {
-	// case BoardGame: {
-	// BoardGame boardGame = new BoardGame();
-	// BeanUtils.copyProperties(mediaRequest, boardGame);
-	// return daoMedia.save(boardGame);
-	// }
-	// case Book: {
-	// Book book = new Book();
-	// BeanUtils.copyProperties(mediaRequest, book);
-	// return daoMedia.save(book);
-	// }
-	// case Magazine: {
-	// Magazine magazine = new Magazine();
-	// BeanUtils.copyProperties(mediaRequest, magazine);
-	// return daoMedia.save(magazine);
-	// }
-	// case Movie: {
-	// Movie movie = new Movie();
-	// BeanUtils.copyProperties(mediaRequest, movie);
-	// return daoMedia.save(movie);
-	// }
-	// case Music: {
-	// Music music = new Music();
-	// BeanUtils.copyProperties(mediaRequest, music);
-	// return daoMedia.save(music);
-	// }
-	// case VideoGame: {
-	// VideoGame videoGame = new VideoGame();
-	// BeanUtils.copyProperties(mediaRequest, videoGame);
-	// return daoMedia.save(videoGame);
-	// }
-	// default:
-	// throw new IllegalArgumentException("Unexpected value: " +
-	// mediaRequest.getTypeMedia());
-	// }
-
-	// }
 
 	// Version avec insertion d'un chemin d'image
 	@PostMapping("/")
@@ -246,7 +194,7 @@ public class MediaApiController {
 
 		// Étape 5 : Stockage de l'image depuis le chemin temporaire vers le chemin
 		// final
-		moveImageToFinalPath(imageFile, temporaryImagePath, imagePath);
+		// moveImageToFinalPath(imageFile, temporaryImagePath, imagePath);
 
 		// Retournez l'objet Media mis à jour
 		return media;
@@ -267,60 +215,66 @@ public class MediaApiController {
 		daoMedia.deleteById(id);
 	}
 
-	@PostMapping("/upload-image")
-	public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
-		try {
-			// Générez un nom de fichier unique basé sur l'UUID
-			String uniqueFileName = UUID.randomUUID().toString() + ".jpg";
+	// @PostMapping("/upload-image")
+	// public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile
+	// file) {
+	// try {
+	// // Générez un nom de fichier unique basé sur l'UUID
+	// String uniqueFileName = UUID.randomUUID().toString() + ".jpg";
 
-			// Chemin complet du fichier image
-			Path filePath = Path.of("/assets/media_pictures/" + uniqueFileName);
+	// // Chemin complet du fichier image
+	// Path filePath = Path.of("/assets/media_pictures/" + uniqueFileName);
 
-			// Assurez-vous que le répertoire d'upload existe, sinon créez-le
-			Path uploadPath = filePath.getParent();
-			if (!Files.exists(uploadPath)) {
-				Files.createDirectories(uploadPath);
-			}
+	// // Assurez-vous que le répertoire d'upload existe, sinon créez-le
+	// Path uploadPath = filePath.getParent();
+	// if (!Files.exists(uploadPath)) {
+	// Files.createDirectories(uploadPath);
+	// }
 
-			// Copiez le fichier image vers le chemin spécifié
-			Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+	// // Copiez le fichier image vers le chemin spécifié
+	// Files.copy(file.getInputStream(), filePath,
+	// StandardCopyOption.REPLACE_EXISTING);
 
-			// Retournez le chemin de stockage de l'image
-			return ResponseEntity.ok(uniqueFileName);
-		} catch (IOException e) {
-			// Gérez les erreurs d'E/S ici, par exemple, en renvoyant une ResponseEntity
-			// avec un code d'erreur
-			return ResponseEntity.status(500).body("Erreur lors de l'enregistrement de l'image.");
-		}
-	}
+	// // Retournez le chemin de stockage de l'image
+	// return ResponseEntity.ok(uniqueFileName);
+	// } catch (IOException e) {
+	// // Gérez les erreurs d'E/S ici, par exemple, en renvoyant une ResponseEntity
+	// // avec un code d'erreur
+	// return ResponseEntity.status(500).body("Erreur lors de l'enregistrement de
+	// l'image.");
+	// }
+	// }
 
-	private void moveImageToFinalPath(MultipartFile imageFile, String temporaryImagePath, String finalImagePath) {
-		try {
-			// Chemin complet du fichier image temporaire
-			Path temporaryImageFilePath = Path.of(temporaryImagePath);
+	// private void moveImageToFinalPath(MultipartFile imageFile, String
+	// temporaryImagePath, String finalImagePath) {
+	// try {
+	// // Chemin complet du fichier image temporaire
+	// Path temporaryImageFilePath = Path.of(temporaryImagePath);
 
-			// Chemin complet du fichier image final
-			Path finalImageFilePath = Path.of(finalImagePath);
+	// // Chemin complet du fichier image final
+	// Path finalImageFilePath = Path.of(finalImagePath);
 
-			// Assurez-vous que le répertoire final existe, sinon créez-le
-			Path finalImageDirectory = finalImageFilePath.getParent();
-			if (!Files.exists(finalImageDirectory)) {
-				Files.createDirectories(finalImageDirectory);
-			}
+	// // Assurez-vous que le répertoire final existe, sinon créez-le
+	// Path finalImageDirectory = finalImageFilePath.getParent();
+	// if (!Files.exists(finalImageDirectory)) {
+	// Files.createDirectories(finalImageDirectory);
+	// }
 
-			// Copiez le fichier image depuis l'emplacement temporaire vers l'emplacement
-			// final
-			Files.copy(imageFile.getInputStream(), finalImageFilePath, StandardCopyOption.REPLACE_EXISTING);
+	// // Copiez le fichier image depuis l'emplacement temporaire vers l'emplacement
+	// // final
+	// Files.copy(imageFile.getInputStream(), finalImageFilePath,
+	// StandardCopyOption.REPLACE_EXISTING);
 
-			// Supprimez le fichier image temporaire s'il est nécessaire
-			if (Files.exists(temporaryImageFilePath)) {
-				Files.delete(temporaryImageFilePath);
-			}
-		} catch (IOException e) {
-			// Gérez les erreurs d'E/S ici, par exemple, en lançant une exception
-			// personnalisée
-			throw new RuntimeException("Erreur lors du déplacement de l'image vers l'emplacement final", e);
-		}
-	}
+	// // Supprimez le fichier image temporaire s'il est nécessaire
+	// if (Files.exists(temporaryImageFilePath)) {
+	// Files.delete(temporaryImageFilePath);
+	// }
+	// } catch (IOException e) {
+	// // Gérez les erreurs d'E/S ici, par exemple, en lançant une exception
+	// // personnalisée
+	// throw new RuntimeException("Erreur lors du déplacement de l'image vers
+	// l'emplacement final", e);
+	// }
+	// }
 
 }

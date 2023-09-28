0a65dd9dd47de9742284e85674f36607c4aa8014
DianeR <diane.reja@gmail.com>
2023-09-28 12:28:48 +0200

    Add to my perso list

---
+++ mediatheque-virtuelle/src/main/java/mediatheque/controller/request/PersoListJoinMediaRequest.java
@@ -0,0 +1,24 @@
+package mediatheque.controller.request;
+
+public class PersoListJoinMediaRequest {
+	
+	private Integer persoListId;
+	private Integer mediaId;
+	
+	public Integer getPersoListId() {
+		return persoListId;
+	}
+	public void setPersoListId(Integer persoListId) {
+		this.persoListId = persoListId;
+	}
+	public Integer getMediaId() {
+		return mediaId;
+	}
+	public void setMediaId(Integer mediaId) {
+		this.mediaId = mediaId;
+	}
+	
+	
+
+
+}
